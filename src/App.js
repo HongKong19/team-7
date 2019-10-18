d6e2603045b3e9a79dedcf7f4208ec323bb4a806
khkchung <46292309+khkchung@users.noreply.github.com>
2019-10-18 12:57:25 +0800

    added family member page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import Copyright from './copyright';
 import Tracker from './components/Tracker';
+import FamilyMember from './components/FamilyMember';
 
 function App() {
   return (
@@ -10,11 +11,13 @@ function App() {
     <BrowserRouter>
 
       <Link to={'/contact'}>go to login</Link> <br></br>
-      <Link to={'/tracker'}>go to track</Link>
+      <Link to={'/tracker'}>go to track</Link> <br></br>
+      <Link to={'/familyMember'}>go to family member</Link>
+
       <Switch>
         <Route path='/contact' component={Copyright}/> 
         <Route path='/tracker' component={Tracker}/> 
-
+        <Route path='/familyMember' component={FamilyMember}/> 
       </Switch>
     </BrowserRouter>
     </div>
