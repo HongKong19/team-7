5b73eddacb70dd6a9ca511fde20a78ad5341bcfe
Rishabh <rhjain100@gmail.com>
2019-10-18 10:24:48 +0800

    Material UI setup, please npm install @material-ui/core and @material-ui/icons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import Copyright from './copyright';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+
+      <Link to={'/contact'}>go to login</Link>
+      <Switch>
+        <Route path='/contact' component={Copyright}/> 
+      </Switch>
+    </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
