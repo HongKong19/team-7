613a1ab75478941cded1530964a877177d9fb094
ShreyaPalit <spalit1003@gmail.com>
2019-10-18 11:48:41 +0800

    Medicine Tracker Table

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import Copyright from './copyright';
+import Tracker from './components/Tracker';
 
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
 
-      <Link to={'/contact'}>go to login</Link>
+      <Link to={'/contact'}>go to login</Link> <br></br>
+      <Link to={'/tracker'}>go to track</Link>
       <Switch>
         <Route path='/contact' component={Copyright}/> 
+        <Route path='/tracker' component={Tracker}/> 
+
       </Switch>
     </BrowserRouter>
     </div>
--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,39 +1,61 @@
 import React from 'react';
 import { Table } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+Table.propTypes = {
+    // Pass in a Component to override default element
+    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+    size: PropTypes.string,
+    bordered: PropTypes.bool,
+    borderless: PropTypes.bool,
+    striped: PropTypes.bool,
+    dark: PropTypes.bool,
+    hover: PropTypes.bool,
+    responsive: PropTypes.bool,
+    // Custom ref handler that will be assigned to the "ref" of the inner <table> element
+    innerRef: PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.string,
+      PropTypes.object
+    ])
+    };
 
 const Example = (props) => {
   return (
-    <Table>
+    <Table dark>
       <thead>
         <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th> </th>
+          <th>Medicine Name</th>
+          <th>Frequency</th>
+          <th>Timing</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
+          <td>Mark Otto</td>
+          <td>2 times</td>
+          <td>Morning, Night</td>
         </tr>
         <tr>
           <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
+          <td>Rose Otto</td>
+          <td>1 time</td>
+          <td>Morning</td>
         </tr>
         <tr>
           <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>@twitter</td>
+          <td>Larry Otto</td>
+          <td>4 times</td>
+          <td>Every 6 hours</td>
         </tr>
       </tbody>
     </Table>
   );
+
+  
+ 
 }
 
 export default Example;
